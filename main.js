50acf6befba86ebf81607ed20faf47719b2a5d2a
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-04-09 15:03:34 -0300

    ADD desktop icons, task bar, clock.

---
+++ main.js
@@ -0,0 +1,19 @@
+window.onload = ()=>{
+    startTime()
+}
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
